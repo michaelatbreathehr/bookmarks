a2c7bcc1a53c601c35ec7aa431fd9a91eaa86dc2
Michael Redgrave <michael@breathehr.com>
2019-08-12 16:45:31 +0100

    add /bookmarks view, test and route before refactor

--- Gemfile
+++ Gemfile
@@ -2,8 +2,8 @@
 
 source "https://rubygems.org"
 
-git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
+git_source(:github) { |repo_name| "https://github.com/#{repo_name}" }
 
-gem 'sinatra'
-gem 'rspec'
 gem 'capybara'
+gem 'rspec'
+gem 'sinatra'
--- app.rb
+++ app.rb
@@ -1,9 +1,17 @@
 require 'sinatra/base'
-
 class BookmarkManager < Sinatra::Base
   get '/' do
-    'Hello World'
-  end
+    'Hello World from bookmarks'
+	end
+	
+	get '/bookmarks' do
+		bookmarks = [
+							"http://www.makersacademy.com",
+							"http://www.destroyallsoftware.com",
+							"http://www.google.com"
+						 ]
+		bookmarks.join
+	end
 
   run! if app_file == $0
 end
